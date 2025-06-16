6919521b87e1ee6a2ddf1b2ca405cf359107626d
Pete Richardson <pete@peterichardson.com>
2025-06-16 12:23:57 -0700

    feat: change font to MonaspiceNe Nerd Font Mono if available

--- ViewHeight/ContentView.swift
+++ ViewHeight/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    private var font: Font {
+        let fontName: String = "MonaspiceNe Nerd Font Mono"
+        let fontSize: CGFloat = 16
+        if NSFont(name: fontName, size: fontSize) != nil {
+            return .custom(fontName, size: fontSize) // Use the custom font if it exists
+        } else {
+            return .system(size: fontSize, weight: .regular, design: .monospaced) // Fallback to monospaced system font
+        }
+    }
     
     @State private var model = TextModel()
     var body: some View {
         TextEditor(text: $model.text)
         .padding()
+        .font(font)
     }
 }
 
