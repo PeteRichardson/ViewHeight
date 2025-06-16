05ab2d3eda2b514674da5bfb2771fc383e5c26d1
Pete Richardson <pete@peterichardson.com>
2025-06-16 09:17:28 -0700

    feat: initial commit.  Hello World

---
+++ ViewHeight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewHeight
+//
+//  Created by Peter Richardson on 6/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewHeight/ViewHeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewHeightApp.swift
+//  ViewHeight
+//
+//  Created by Peter Richardson on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ViewHeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
